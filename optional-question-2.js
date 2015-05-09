db0684121abab7f40a8e0053ce9a5168e3de0af7
Eddie Naff <eddienaff@gmail.com>
2015-05-09 00:01:05 -0700

    Begin question 2

---
+++ optional-question-2.js
@@ -0,0 +1,23 @@
+// ASTRONAUTS QUIZ
+
+var moonWalkers = [
+  "Neil Armstrong",
+  "Buzz Aldrin",
+  "Pete Conrad",
+  "Alan Bean",
+  "Alan Shepard",
+  "Edgar Mitchell",
+  "David Scott",
+  "James Irwin",
+  "John Young",
+  "Charles Duke",
+  "Eugene Cernan",
+  "Harrison Schmitt"
+];
+
+function alphabetizer(names) {
+    // Your code goes here!
+}
+
+// Try logging your results to test your code!
+console.log(alphabetizer(moonWalkers));
