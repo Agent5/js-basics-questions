ef4473b4ecac2045568f3ae6f5f00404e231d8a7
Eddie Naff <eddienaff@gmail.com>
2015-05-08 20:28:05 -0700

    Reset the question

--- optional-question-1.js
+++ optional-question-1.js
@@ -1,37 +1,19 @@
 // OPTIONAL CHALLENGE QUESTION #1
 
-var nonNumerics = [];
-
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function getRelationship(x, y) {
-
-    for (var i = 0; i < 2; i++) {
-        if (isNumeric(arguments[i]) === false) {
-            nonNumerics.push(arguments[i]);
-        }
-    }
-
-    if (nonNumerics.length > 1) {
-        return "Can't compare relationships because " + x + " and " + y + " are not numbers";
-    } else if (nonNumerics.length === 1) {
-        return "Can't compare relationships because " + nonNumerics[0] + " is not a number";
-    } else if (x < y) {
-        // console.log(x + " is > " + y);
-        var relationship = x + " is LESS THAN " + y;
-        return relationship;
-    } else if (x > y) {
-        var relationship = x + " is GREATER THAN " + y;
-        return relationship;
-    } else if (x === y) {
-
-        var relationship = x + " is EQUAL TO " + y;
-        return relationship;
-    }
+    // Your code goes here!
 }
 
-var rel = getRelationship(3, 3);
-
-console.log(rel);
+// Try logging these functions to test your code!
+console.log(getRelationship(1,4));
+console.log(getRelationship(1,1));
+console.log(getRelationship("that",2));
+console.log(getRelationship("this"," something else"));
+console.log(getRelationship(3));
+console.log(getRelationship("hi"));
+console.log(getRelationship(NaN));
+console.log(getRelationship(NaN, undefined));
